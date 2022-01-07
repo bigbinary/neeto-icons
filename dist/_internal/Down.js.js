196df6920abc90c65a60404ef7a007f299a9fd66
Ajil Paul <36255991+AJIL-PAUL@users.noreply.github.com>
2022-01-07 13:20:09 +0530

    Bring tree shaking (#69)
    * Added rollup for esm module bundler
    
    * Enabled tree shaking and updated version

---
+++ dist/_internal/Down.js.js
@@ -0,0 +1,2 @@
+import{P as e,_ as r,R as o,a as t}from"./index.js";var s=["size","color","strokeWidth"];function i(e){var i=e.size,n=e.color,a=e.strokeWidth,d=r(e,s);return o.createElement("svg",t({viewBox:"0 0 24 24",fill:"none",height:i,width:i},d),o.createElement("path",{d:"M6 9L12 15L18 9",stroke:n,strokeWidth:a,strokeLinecap:"round",strokeLinejoin:"round"}))}i.defaultProps={color:"currentColor",size:24,strokeWidth:"1.5"},i.propTypes={color:e.string,size:e.oneOfType([e.string,e.number]),strokeWidth:e.oneOfType([e.string,e.number])};export{i as Down};
+//# sourceMappingURL=Down.js.js.map
---
+++ dist/_internal/RadioInactive.js.js
@@ -0,0 +1,2 @@
+import{P as e,_ as r,R as o,a as t}from"./index.js";var s=["size","color","strokeWidth"];function i(e){var i=e.size,n=e.color,c=e.strokeWidth,l=r(e,s);return o.createElement("svg",t({viewBox:"0 0 24 24",fill:"none",height:i,width:i},l),o.createElement("circle",{cx:"12",cy:"12",r:"8.25",stroke:n,strokeWidth:c}))}i.defaultProps={color:"currentColor",size:24,strokeWidth:"1.5"},i.propTypes={color:e.string,size:e.oneOfType([e.string,e.number]),strokeWidth:e.oneOfType([e.string,e.number])};export{i as RadioInactive};
+//# sourceMappingURL=RadioInactive.js.js.map
