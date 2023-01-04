487201cdc7365f5e0d81bd3017487d76f7eae90a
Amaljith K <amaljith.k@bigbinary.com>
2023-01-04 13:08:31 +0530

    Added both icons and logos in dev app

--- example/src/Dashboard/IconsList/index.js
+++ example/src/Dashboard/IconsList/index.js
@@ -2,14 +2,12 @@ import React from "react";
 
 import IconItem from "./IconItem";
 
-import * as icons from "../../../../lib/icons";
-
-export default function IconsList({ searchTerm }) {
+export default function IconsList({ searchTerm, icons, listName }) {
   const filteredIconsList = Object.keys(icons).filter((name) =>
     name.toLowerCase().includes(searchTerm.toLowerCase())
   );
   return filteredIconsList.length === 0 ? (
-    <div className="px-8 text-sm text-gray-600">No icons found.</div>
+    <div className="px-8 text-sm text-gray-600">No {listName} found.</div>
   ) : (
     <div className="grid w-full grid-cols-3 gap-4 px-8 py-4 md:grid-cols-6 lg:grid-cols-10">
       {filteredIconsList.map((key) => (
--- example/src/Dashboard/index.js
+++ example/src/Dashboard/index.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import Header from "./Header";
 import IconsList from "./IconsList";
 import { Toaster } from "react-hot-toast";
+import * as icons from "../../../lib/icons";
+import * as logos from "../../../lib/logos";
 
 function Dashboard() {
   const [searchTerm, setSearchTerm] = useState("");
@@ -10,8 +12,9 @@ function Dashboard() {
       <Toaster position="bottom-center" />
       <Header searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       <h2 className="px-8 pt-4">Logos</h2>
+      <IconsList searchTerm={searchTerm} icons={logos} listName="logos" />
       <h2 className="px-8 pt-4">Icons</h2>
-      <IconsList searchTerm={searchTerm} />
+      <IconsList searchTerm={searchTerm} icons={icons} listName="icons" />
     </>
   );
 }
