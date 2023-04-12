00829f2cec4a13f6e1814aadc974104d2ea78ea7
Goutham Subramanyam <gsn.goutham@gmail.com>
2023-04-12 11:23:56 +0530

    Updated Computer, Tablet and Mobile icons

--- src/icons/Mobile.js
+++ src/icons/Mobile.js
@@ -3,22 +3,20 @@ import React from "react";
 import PropTypes from "prop-types";
 
 export const Mobile = ({ size, color, strokeWidth, ...other }) => (
-  <svg width={size} height={size} viewBox="0 0 12 18" fill="none" {...other}>
-    <rect
-      x="0.75"
-      y="0.75"
-      width="10.5"
-      height="16.5"
-      rx="2.25"
+  <svg width={size} height={size} viewBox="0 0 24 24" fill="none" {...other}>
+    <path
+      d="M18 19C18 19.5304 17.7893 20.0391 17.4142 20.4142C17.0391 20.7893 16.5304 21 16 21H8C7.46957 21 6.96086 20.7893 6.58579 20.4142C6.21071 20.0391 6 19.5304 6 19V5C6 4.46957 6.21071 3.96086 6.58579 3.58579C6.96086 3.21071 7.46957 3 8 3H16C16.5304 3 17.0391 3.21071 17.4142 3.58579C17.7893 3.96086 18 4.46957 18 5V19Z"
       stroke={color}
       strokeWidth={strokeWidth}
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
-    <line
-      y1="13.25"
-      x2="12"
-      y2="13.25"
+    <path
+      d="M14.5 3V5C14.5 5.13261 14.4473 5.25979 14.3536 5.35355C14.2598 5.44732 14.1326 5.5 14 5.5H10C9.86739 5.5 9.74021 5.44732 9.64645 5.35355C9.55268 5.25979 9.5 5.13261 9.5 5V3H14.5Z"
       stroke={color}
       strokeWidth={strokeWidth}
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
   </svg>
 );
