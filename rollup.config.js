394a7f286fc0a78fd7fb849ff281e21a52606343
Sruthy J Mallya <53861843+sruthy-j-mallya@users.noreply.github.com>
2023-07-03 19:51:22 +0530

    Updated rollup config to generate cjs bundle (#205)

--- rollup.config.js
+++ rollup.config.js
@@ -11,7 +11,7 @@ const DIST_PATH = path.resolve(__dirname, "dist");
 export const extensions = [".js", ".jsx"];
 
 // Compile index & compile separately every elements exported by it.
-const input = Object.fromEntries([
+const input = [
   ["neeto-icons", "./generate/icons/index.js"],
   ["neeto-logos", "./generate/logos/index.js"],
   ["app-icons", "./generate/app-icons/index.js"],
@@ -21,36 +21,40 @@ const input = Object.fromEntries([
     const componentName = path.basename(componentPath);
     return [componentName, componentPath];
   }),
-]);
+];
 
 
 const formats = ["esm", "cjs"];
 
-const output = formats.map((format) => ({
-  format,
-  sourcemap: true,
-  dir: path.join(DIST_PATH),
-}));
+const plugins = [
+  // generate components
+  generateComponents(componentGenerationConfig),
+  // Analyze created bundle.
+  analyze(),
+  // Resolve source files.
+  resolve({ browser: true, extensions }),
+  // Resolve commonjs dependencies.
+  commonjs({ include: /node_modules/ }),
+  babel({
+    extensions,
+    exclude: /node_modules/,
+    presets: [
+      ["@babel/preset-env", { targets: "defaults" }],
+      "@babel/preset-react",
+    ],
+  }),
+];
 
-export default {
-  input,
-  output,
-  plugins: [
-    // generate components
-    generateComponents(componentGenerationConfig),
-    // Analyze created bundle.
-    analyze(),
-    // Resolve source files.
-    resolve({ browser: true, extensions }),
-    // Resolve commonjs dependencies.
-    commonjs({ include: /node_modules/ }),
-    babel({
-      extensions,
-      exclude: /node_modules/,
-      presets: [
-        ["@babel/preset-env", { targets: "defaults" }],
-        "@babel/preset-react",
-      ],
-    }),
-  ],
-};
+const getOutputFilename = (name, format) =>
+ format === "esm" ? `dist/${name}.js` : `dist/${name}.cjs.js`
+
+export default input.map(([filename, pathname]) => ({
+  input: pathname,
+  plugins,
+  output: formats.map((format) => ({
+    assetFileNames: "[name][extname]",
+    file: getOutputFilename(filename, format),
+    format,
+    sourcemap: true,
+  }))
+}));
