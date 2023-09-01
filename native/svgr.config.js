339daa8d372e482a88a5f1dcda13c9bb0d615ead
Mohit <mohitharshan.mec@gmail.com>
2023-09-01 18:34:02 +0530

    added color prop support

---
+++ native/svgr.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  svgProps: {
+    width: "{props.size}",
+    height: "{props.size}",
+    fill: "{props.color}",
+  },
+  replaceAttrValues: {
+    "#68737D": "{props.color}",
+  },
+};
