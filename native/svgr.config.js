cde073e41eb0babebf90b56b73e936720511af0b
Mohit <mohitharshan.mec@gmail.com>
2023-09-04 16:37:17 +0530

    replaced all colors

--- native/svgr.config.js
+++ native/svgr.config.js
@@ -3,9 +3,28 @@ module.exports = {
     width: "{props.size}",
     height: "{props.size}",
   },
-  replaceAttrValues: {
-    "#68737D": "{props.color}",
-    "#49545C": "{props.color}",
-    "#49545C": "{props.color}",
-  },
+  replaceAttrValues: generateReplaceAttrValues(),
 };
+
+function generateReplaceAttrValues() {
+  const colorValues = [
+    "#68737D",
+    "#49545C",
+    "#323232",
+    "#00BA88",
+    "#F56A58",
+    "#BB121A",
+    "#4B2876",
+    "#2F3941",
+    "#276EF1",
+    "#107154",
+    "#fff",
+    "#87929D",
+    "#F3CD82",
+  ];
+
+  return colorValues.reduce((result, value) => {
+    result[value] = "{props.color}";
+    return result;
+  }, {});
+}
