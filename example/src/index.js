f13d9b3f3033341ac7ceb96c759936402a9564fe
Neenu Chacko <45137335+neenu-chacko@users.noreply.github.com>
2023-09-19 09:37:38 +0530

    Updated client rendering API

--- example/src/index.js
+++ example/src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 
 import Dashboard from "./Dashboard";
 import "./index.css";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
   <React.StrictMode>
     <Dashboard />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
