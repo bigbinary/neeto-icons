7126536e7f5943fc444b07fb7b9462bd315b6b57
Ajmal Noushad <ajufhn@gmail.com>
2023-12-13 15:12:10 +0530

    Support for selected icon as URL query param

--- example/src/Dashboard/IconsList/IconItem.js
+++ example/src/Dashboard/IconsList/IconItem.js
@@ -1,29 +1,24 @@
-import React from "react";
+import React, { forwardRef, useEffect, useRef } from "react";
+import classNames from "classnames";
 
-import copy from "copy-to-clipboard";
-import toast from "react-hot-toast";
+const IconItem = ({ name, Icon, onSelect, isSelected }, ref) => {
 
-export default function IconItem({ name, Icon }) {
-  const copyName = () => {
-    copy(name);
-    toast(`${name} has been copied to clipboard.`, {
-      icon: "🎉",
-      style: {
-        borderRadius: "4px",
-        background: "#1e1e20",
-        color: "#fff",
-        padding: "4px 8px",
-        fontSize: 14,
-      },
-    });
-  };
+
   return (
     <div
-      onClick={copyName}
-      className="flex flex-col items-center justify-center col-span-1 p-8 transition-all transform bg-white border rounded cursor-pointer hover:bg-gray-100"
+      ref={ref}
+      onClick={() => onSelect(name)}
+      className={classNames(
+        "flex flex-col items-center justify-center col-span-1 p-8 transition-all transform bg-white border rounded cursor-pointer hover:bg-gray-100",
+        {
+          "border-blue-500": isSelected,
+        }
+      )}
     >
       <Icon size={24} color="#68737D" />
       <div className="mt-2 text-xs text-gray-700">{name}</div>
     </div>
   );
 }
+
+export default forwardRef(IconItem);
--- example/src/Dashboard/IconsList/index.js
+++ example/src/Dashboard/IconsList/index.js
@@ -1,9 +1,46 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 import IconItem from "./IconItem";
 import { DEFAULT_CLASS_NAME } from "./constants";
+import copy from "copy-to-clipboard";
+import toast from "react-hot-toast";
+const noop = () => {};
 
-export default function IconsList({ searchTerm, icons, listName, className = DEFAULT_CLASS_NAME }) {
+export default function IconsList({
+  searchTerm,
+  icons,
+  listName,
+  className = DEFAULT_CLASS_NAME,
+}) {
+  const ref = useRef();
+  const [selectedIcon, setSelectedIcon] = useState(
+    new URL(window.location).searchParams.get("selectedIcon")
+  );
+  const onSelect = (name) => {
+    copy(name);
+    setSelectedIcon(`${listName}-${name}`);
+    toast(`${name} has been copied to clipboard.`, {
+      icon: "🎉",
+      style: {
+        borderRadius: "4px",
+        background: "#1e1e20",
+        color: "#fff",
+        padding: "4px 8px",
+        fontSize: 14,
+      },
+    });
+    const url = new URL(window.location);
+    url.searchParams.set("selectedIcon", `${listName}-${name}`);
+    window.history.pushState({}, "", url);
+  };
+
+  useEffect(() => {
+    ref.current?.scrollIntoView({
+      behavior: "smooth",
+      block: "center",
+    });
+  }, []);
+
   const filteredIconsList = Object.keys(icons).filter((name) =>
     name.toLowerCase().includes(searchTerm.toLowerCase())
   );
@@ -11,9 +48,19 @@ export default function IconsList({ searchTerm, icons, listName, className = DEF
     <div className="px-8 text-sm text-gray-600">No {listName} found.</div>
   ) : (
     <div className={className}>
-      {filteredIconsList.map((key) => (
-        <IconItem key={key} Icon={icons[key]} name={key} />
-      ))}
+      {filteredIconsList.map((key) => {
+        const isSelected = `${listName}-${key}` === selectedIcon;
+        return (
+          <IconItem
+            ref={isSelected ? ref : noop}
+            key={key}
+            Icon={icons[key]}
+            name={key}
+            onSelect={onSelect}
+            isSelected={isSelected}
+          />
+        );
+      })}
     </div>
   );
 }
