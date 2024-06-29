f4dc54367c558eb624998613914f80b19386e361
Abhay V Ashokan <abhayvashokan@gmail.com>
2024-06-29 23:49:46 +0530

    Updated the icon templates to default export the icons

--- build/templates/icon-template.mjs
+++ build/templates/icon-template.mjs
@@ -1,9 +1,11 @@
 const iconTemplate = ({ componentName, jsx }, { tpl }) => tpl`
 import React from 'react';
 
-export const ${componentName} = ({ size=24, color="currentColor", strokeWidth=1.5, ...props }) => {
+const ${componentName} = ({ size=24, color="currentColor", strokeWidth=1.5, ...props }) => {
   return ${jsx};
 }
+
+export default ${componentName};
 `;
 
 export default iconTemplate;
--- build/templates/typeface-logo-template.mjs
+++ build/templates/typeface-logo-template.mjs
@@ -1,9 +1,11 @@
 const typefaceLogoTemplate = ({ componentName, jsx }, { tpl }) => tpl`
 import React from "react";
 
-export const ${componentName} = ({width="100%", height=36, ...props}) => {
+const ${componentName} = ({width="100%", height=36, ...props}) => {
   return ${jsx};
 }
+
+export default ${componentName};
 `;
 
 export default typefaceLogoTemplate;
